61a6b85504a3d056a1118006487db1d82682229e
Nihal-7 <rajbalanihal@gmail.com>
2023-08-02 15:31:54 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navbar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+
+
+function App() {
+
+  
+  return (
+    <Router>
+      <div className="App">
+        <Navbar /> 
+       
+      </div>
+    </Router>
+  );
+}
+
+export default App;
