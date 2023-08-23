4f54d6128ada007353f4e20e2eb6150db66e31ff
prateek-baheti <87934232+prateek-baheti@users.noreply.github.com>
2023-08-23 22:49:41 +0530

    Update Menu.js
    added the space

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -13,7 +13,7 @@ export default function Menu() {
     <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
     <form className="d-flex " role="search">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" size="70"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
+        <button className="btn btn-outline-success" type="submit">Search </button>
       </form>
       
       
